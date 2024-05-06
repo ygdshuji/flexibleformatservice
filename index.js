57742cd10fe6047031a0dbb1b9d12e450f239e49
ygdshuji <gs1rnsawadi@hotmail.com>
2024-05-07 02:31:17 +0700

    aptos task

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-function maxDepth(root) {
-  if (!root) return 0;
-  return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+function rob(nums) {
+  if (nums.length === 1) return nums[0];
+  const robRange = (start, end) => {
+    let prevMax = 0;
+    let currMax = 0;
+    for (let i = start; i <= end; i++) {
+      const temp = currMax;
+      currMax = Math.max(currMax, prevMax + nums[i]);
+      prevMax = temp;
+    }
+    return currMax;
+  };
+  return Math.max(robRange(0, nums.length - 2), robRange(1, nums.length - 1));
 }
