83746baf2fd6dd158ad1887d27e09cdfc5e29769
ygdshuji <gs1rnsawadi@hotmail.com>
2024-05-10 02:11:48 +0700

    demo commit

--- index.js
+++ index.js
@@ -1,14 +1,2 @@
-function wordBreak(s, wordDict) {
-  const wordSet = new Set(wordDict);
-  const dp = new Array(s.length + 1).fill(false);
-  dp[0] = true;
-  for (let end = 1; end <= s.length; end++) {
-    for (let start = 0; start < end; start++) {
-      if (dp[start] && wordSet.has(s.substring(start, end))) {
-        dp[end] = true;
-        break;
-      }
-    }
-  }
-  return dp[s.length];
-}
+const countOccurrences = (arr, value) =>
+  arr.reduce((a, v) => (v === value ? a + 1 : a), 0);
