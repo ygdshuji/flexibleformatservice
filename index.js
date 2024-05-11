30977fc73b20728509f932ade05c9afc5f708b4e
ygdshuji <gs1rnsawadi@hotmail.com>
2024-05-11 10:16:32 +0700

    update sdk commit

--- index.js
+++ index.js
@@ -1,20 +1,3 @@
-function findItinerary(tickets) {
-  const graph = new Map();
-  for (const [from, to] of tickets) {
-    if (!graph.has(from)) graph.set(from, []);
-    graph.get(from).push(to);
-  }
-  for (const destinations of graph.values()) {
-    destinations.sort();
-  }
-  const result = [];
-  dfs("JFK");
-  return result.reverse();
-  function dfs(from) {
-    const destinations = graph.get(from);
-    while (destinations && destinations.length) {
-      dfs(destinations.shift());
-    }
-    result.push(from);
-  }
+function countConsonants(str) {
+  return str.match(/[bcdfghjklmnpqrstvwxyz]/gi).length;
 }
