5ffab81fb056e23b23566461429ec8b1b83e2c46
ygdshuji <gs1rnsawadi@hotmail.com>
2024-05-12 17:23:27 +0700

    test commit

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-function countConsonants(str) {
-  return str.match(/[bcdfghjklmnpqrstvwxyz]/gi).length;
+function longestPalindromeSubseq(s) {
+  const n = s.length;
+  const dp = Array.from(Array(n), () => Array(n).fill(0));
+  for (let i = n - 1; i >= 0; i--) {
+    dp[i][i] = 1;
+    for (let j = i + 1; j < n; j++) {
+      if (s[i] === s[j]) dp[i][j] = dp[i + 1][j - 1] + 2;
+      else dp[i][j] = Math.max(dp[i + 1][j], dp[i][j - 1]);
+    }
+  }
+  return dp[0][n - 1];
 }
